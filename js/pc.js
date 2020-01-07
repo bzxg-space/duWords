ac5fca770b0bac4749ebc00bdf5010dd74f66cff
bzxg-space <58965227+bzxg-space@users.noreply.github.com>
2020-01-07 19:11:35 +0800

    Add files via upload

---
+++ js/pc.js
@@ -0,0 +1,24 @@
+var i;
+var word = document.getElementsByTagName("p")[0];
+refreshOpen();
+
+//刷新
+function refreshOpen() {
+	while (true) {
+		num = Math.ceil(Math.random() * words.length);
+		if (num >= 0 && num < words.length && i != num) {
+			i = num - 1;
+			break;
+		}
+	}
+	newOpen();
+}
+//设置
+function newOpen() {
+	i++;
+	if (i == words.length) {
+		i = 0;
+	}
+	word.innerHTML = words[i];
+	console.log(i);
+}
---
+++ js/phone.js
@@ -0,0 +1,26 @@
+var i = 0;
+var word = document.getElementById('word');
+change();
+//改变
+function change() {
+	while (true) {
+		num = Math.floor(Math.random() * words.length);
+		console.log(num);
+		if (i != num) {
+			i = num;
+			break;
+		}
+	}
+	word.innerHTML = words[i];
+}
+//复制
+function copy() {
+	var textArea = document.createElement("textarea");
+	textArea.innerHTML = word.innerHTML;
+	document.body.appendChild(textArea);
+	textArea.select();
+	document.execCommand('copy');
+	toast('复制成功 : )');
+	textArea.parentNode.removeChild(textArea);
+	//document.body.removeChild(textArea);
+}
